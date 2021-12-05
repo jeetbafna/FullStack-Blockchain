c2ff7873a417b54ebe8733c23d5306d4b3e17f5d
Jeet Bafna <jeetanil97@gmail.com>
2021-12-04 19:56:59 -0600

    Difficulty and nonce test for Proof of work

--- blockchain.js
+++ blockchain.js
@@ -17,7 +17,7 @@ class Blockchain {
 
 	replaceChain(chain) {
 
-		
+
 		if(chain.length <= this.chain.length){
 			console.error('The incoming chain must be longer');
 			return;
@@ -38,12 +38,12 @@ class Blockchain {
 
 
 		for(let i=1; i<chain.length; i++){
-			const { timestamp, lastHash, hash, data } = chain[i];
+			const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i];
 			const actualLastHash = chain[i-1].hash;
 
 			if(lastHash !== actualLastHash) return false;
 
-			const validatedHash = cryptoHash(timestamp, lastHash, data);
+			const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
 
 			if(hash !== validatedHash) return false;
 		}
