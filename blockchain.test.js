5cf82f5a3075b1be2bcc0060d49486e684a111b3
Jeet Bafna <jeetanil97@gmail.com>
2021-12-04 21:14:03 -0600

    Preventing difficulty jumps

--- blockchain.test.js
+++ blockchain.test.js
@@ -1,5 +1,6 @@
 const Blockchain = require('./blockchain');
 const Block = require('./block');
+const cryptoHash = require('./crypto-hash')
 
 describe('Blockchain', () => {
 	let blockchain, newChain, originalChain;
@@ -58,6 +59,25 @@ describe('Blockchain', () => {
 					expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
 				})
 			});
+
+			describe('and the chain contains a block with jumped difficulty', () => {
+				it('returns false', () => {
+					const lastBlock = blockchain.chain[blockchain.chain.length-1];
+
+					const lastHash = lastBlock.hash;
+					const timestamp = Date.now();
+					const nonce =0;
+					const data=[];
+					const difficulty = lastBlock.difficulty-3;
+
+					const hash = cryptoHash(timestamp, lastHash, difficulty, nonce, data);
+					const badBlock = new Block({ timestamp, lastHash, hash, difficulty, nonce, data });
+
+					blockchain.chain.push(badBlock);
+
+					expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+				})
+			})
 
 			describe('and the chain does not contain any invalid blocks', () => {
 				it('returns true', () => {
