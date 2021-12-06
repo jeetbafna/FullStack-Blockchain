74f5ee460a4a85b9a2d1811e82800769654bd45a
Jeet Bafna <jeetanil97@gmail.com>
2021-12-05 21:39:07 -0600

    Adding syncronization beween peers

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const PubSub = require('./pubsub');
@@ -7,7 +8,12 @@ const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
 
-setTimeout(() => pubsub.broadcastChain(), 1000)
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
+
+setTimeout(() => pubsub.broadcastChain(), 1000);
+
+
 
 app.use(bodyParser.json());
 
@@ -25,8 +31,22 @@ app.post('/api/mine', (req, res) => {
 	res.redirect('/api/blocks');
 });
 
+const syncChains = () => {
+	request({url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
+		if(error){
+			console.log(error);
+		}
+		if(!error && response.statusCode === 200){
+			const rootChain = JSON.parse(body);
 
-const DEFAULT_PORT = 3000;
+			console.log('replace chain on a sync with', rootChain);
+			blockchain.replaceChain(rootChain);
+		}
+	});
+}
+
+
+
 
 let PEER_PORT;
 
@@ -38,4 +58,6 @@ const PORT = PEER_PORT || DEFAULT_PORT;
 
 app.listen(PORT, () => {
 	console.log(`listening at localhost:${PORT}`);
+
+	syncChains();
 });
