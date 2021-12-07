04f773003a9a9357f1d021cce8c09cedeed9263e
Jeet Bafna <jeetanil97@gmail.com>
2021-12-07 17:51:36 -0600

    Adding api calls to create transactions

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const request = require('request');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
 
 const app = express();
 const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
 const pubsub = new PubSub({ blockchain });
 
 const DEFAULT_PORT = 3000;
@@ -30,6 +34,20 @@ app.post('/api/mine', (req, res) => {
 
 	res.redirect('/api/blocks');
 });
+
+
+app.post('/api/transact', (req, res) => {
+	const { amount, recipient } = req.body;
+
+	const transaction = wallet.createTransaction({ recipient, amount });
+
+	transactionPool.setTransaction(transaction);
+
+	console.log('transactionPool', transactionPool);
+
+	res.json({ transaction });
+});
+
 
 const syncChains = () => {
 	request({url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
