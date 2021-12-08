8e45d18714d1a48a2428dee272d2a39aa43999f7
Jeet Bafna <jeetanil97@gmail.com>
2021-12-08 17:09:54 -0600

    Clearing mined transactions in all peers

--- blockchain/index.js
+++ blockchain/index.js
@@ -15,7 +15,7 @@ class Blockchain {
 		this.chain.push(newBlock);
 	}
 
-	replaceChain(chain) {
+	replaceChain(chain, onSuccess) {
 
 
 		if(chain.length <= this.chain.length){
@@ -26,6 +26,9 @@ class Blockchain {
 		if(!Blockchain.isValidChain(chain)){
 			console.error("The incoming chain must be valid")
 			return;
+		}
+		if(onSuccess){
+			onSuccess();
 		}
 
 		console.log("replacing chain with", chain);
