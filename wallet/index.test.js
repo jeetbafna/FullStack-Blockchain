a074f3f90b85501b4895ad8f0ef904c153b07022
Jeet Bafna <jeetanil97@gmail.com>
2021-12-12 19:39:18 -0600

    Taking care of more balance calculation in a blockchain

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -138,6 +138,62 @@ describe('wallet', () => {
 				).toEqual(STARTING_BALANCE + transaction1.outputMap[wallet.publicKey] + transaction2.outputMap[wallet.publicKey]);
 			});
 
+			describe('and the wallet has made a transaction', () => {
+				let recentTransaction;
+
+				beforeEach(() => {
+					recentTransaction = wallet.createTransaction({
+						recipient: 'foo123',
+						amount:30
+					});
+
+					blockchain.addBlock({ data: [recentTransaction] });
+				});
+
+				it('returns the output amount of the recent transaction', () => {
+					expect(
+						Wallet.calculateBalance({
+							chain: blockchain.chain,
+							address: wallet.publicKey
+						})
+					).toEqual(recentTransaction.outputMap[wallet.publicKey]);
+				});
+
+				describe('and there are outputs next to and after the recent Transaction', () => {
+					let sameBlockTransaction, nextBlockTransaction;
+
+					beforeEach(() => {
+						recentTransaction = wallet.createTransaction({
+							recipient: 'later-foo',
+							amount:70
+						});
+
+						sameBlockTransaction = Transaction.rewardTransaction({ minerWallet: wallet });
+
+						blockchain.addBlock({ data: [recentTransaction, sameBlockTransaction] });
+
+						nextBlockTransaction = new Wallet().createTransaction({
+							recipient: wallet.publicKey,
+							amount:75
+						});
+
+						blockchain.addBlock({ data: [nextBlockTransaction] });
+					});
+
+					it('includes the output amounts in the returned balance', () => {
+						expect(
+							Wallet.calculateBalance({
+								chain: blockchain.chain,
+								address: wallet.publicKey
+							})
+						).toEqual(
+							recentTransaction.outputMap[wallet.publicKey] +
+							sameBlockTransaction.outputMap[wallet.publicKey] + 
+							nextBlockTransaction.outputMap[wallet.publicKey]
+						);
+					});
+				});
+			});
 		});
 	});
 });
