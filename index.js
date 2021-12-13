52629703baf875e546ccec6c0fa5607456f8f2d5
Jeet Bafna <jeetanil97@gmail.com>
2021-12-12 19:45:20 -0600

    Creating an endpoint for accessing wallet info

--- index.js
+++ index.js
@@ -70,6 +70,14 @@ app.get('/api/mine-transactions', (req, res) => {
 	transactionMiner.mineTransactions();
 
 	res.redirect('/api/blocks');
+});
+
+app.get('/api/wallet-info', (req, res) => {
+	const address = wallet.publicKey;
+	res.json({ 
+		address,
+		balance: Wallet.calculateBalance({ chain: blockchain.chain, address}) 
+	});
 })
 
 const syncWithRootState = () => {
