6b0e7ddbb8df53d2ed46b24d55431896ae1d10a7
Jeet Bafna <jeetanil97@gmail.com>
2021-12-15 15:46:14 -0600

    Making a call to backend and using the information in then App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+	state = { walletInfo: {} };
+
+	componentDidMount(){
+		fetch('http://localhost:3000/api/wallet-info')
+		.then(response => response.json())
+		.then(json => this.setState({ walletInfo: json }));
+	}
 	render() {
+		const { address, balance } = this.state.walletInfo;
+
+		
 		return (
 			<div>
-				Welcome to the Blockchain!!
+				<div>
+					Welcome to the Blockchain!!
+				</div>
+				<div>Address: {address}</div>
+				<div>Balance: {balance}</div>
 			</div>
 		);
 	}
