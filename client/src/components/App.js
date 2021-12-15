d073af9d1c5db5dbd57d53ca4eb5098c66503b8d
Jeet Bafna <jeetanil97@gmail.com>
2021-12-15 15:21:50 -0600

    Adding a component

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				Welcome to the Blockchain!!
+			</div>
+		);
+	}
+}
+
+
+export default App;
