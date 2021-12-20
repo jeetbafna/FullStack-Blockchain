d7e605860c47dc88f6ad64b0ce68220e5c5caaad
Jeet Bafna <jeetanil97@gmail.com>
2021-12-20 13:03:16 -0600

    Creating a transaction component

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
+import Transaction from './Transaction';
 
 
 class Block extends Component {
@@ -19,7 +20,14 @@ class Block extends Component {
 		if(this.state.displayTransaction) {
 			return(
 				<div>
-					{JSON.stringify(data)}
+					{
+						data.map(transaction => (
+							<div key={transaction.id}>
+								<hr />
+								<Transaction transaction={transaction} />
+							</div>
+						))
+					}
 					<br />
 					<Button bsStyle="danger" bsSize="small" onClick={this.toggleTransaction}>Show Less</Button>
 				</div>
