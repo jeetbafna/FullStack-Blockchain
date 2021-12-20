9acee1fd1cffdc1b972fb4e2bed075f647f447ae
Jeet Bafna <jeetanil97@gmail.com>
2021-12-20 12:48:23 -0600

    toggling the display of information in the block'

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 
 class Block extends Component {
-	render() {
-		const { timestamp, hash, data } = this.props.block;
+	state = { displayTransaction: false };
 
-		const hashDisplay = `${hash.substring(0,15)}...`;
+	toggleTransaction = () => {
+		this.setState({ displayTransaction: !this.state.displayTransaction });
+	}
+	
+	get displayTransaction() {
+		const { data } = this.props.block;
 
 		const strigifiedData = JSON.stringify(data);
 
 		const dataDisplay =strigifiedData.length > 35 ? `${strigifiedData.substring(0,35)}...` : strigifiedData;
 
+		if(this.state.displayTransaction) {
+			return(
+				<div>
+					{JSON.stringify(data)}
+					<br />
+					<Button bsStyle="danger" bsSize="small" onClick={this.toggleTransaction}>Show Less</Button>
+				</div>
+
+			);
+		}
+
+		return (
+			<div>
+				<div>Data: {dataDisplay}</div>
+				<Button bsStyle="danger" bsSize="small" onClick={this.toggleTransaction}>Show More</Button>
+			</div>
+		);
+	}
+
+	render() {
+		const { timestamp, hash } = this.props.block;
+
+		const hashDisplay = `${hash.substring(0,15)}...`;
+
+		
+
 		return(
 			<div className="Block">
 				<div>Hash: {hashDisplay} </div>
 				<div>Timestamp: {new Date(timestamp).toLocaleString()}</div>
-				<div>Data: {dataDisplay}</div>
+				
+				{this.displayTransaction}
 			</div>
 		)
 	}
