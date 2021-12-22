301849bc5bef81539861ce3532e858644dfddc69
Jeet Bafna <jeetanil97@gmail.com>
2021-12-22 14:13:57 -0600

    Adding a UI for known addresses'

--- client/src/components/ConductTransaction.js
+++ client/src/components/ConductTransaction.js
@@ -5,7 +5,13 @@ import history from '../history';
 
 
 class ConductTransaction extends Component {
-	state= { recipient: '', amount: 0 };
+	state= { recipient: '', amount: 0, knownAddresses: [] };
+
+	componentDidMount() {
+		fetch(`${document.location.origin}/api/known-addresses`)
+		.then(response => response.json())
+		.then(json => this.setState({ knownAddresses: json }))
+	}
 
 	updateRecipient = event => {
 		this.setState({ recipient: event.target.value });
@@ -35,6 +41,19 @@ class ConductTransaction extends Component {
 			<div className='ConductTransaction'>
 				<Link to='/'>Home</Link>
 				<h3>Conduct a Transaction</h3>
+				<br />
+				<h4>Known Addresses</h4>
+				{
+					this.state.knownAddresses.map(knwonAddress => {
+						return(
+							<div key={knwonAddress}>
+								<div>{knwonAddress}</div>
+								<br />
+							</div>
+						);
+					})
+				}
+				<br />
 				<FormGroup>
 					<FormControl 
 						input='text'
