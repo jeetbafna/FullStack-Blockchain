2000a7df1f4f56fddb55aae52269b6ebcb6d0ba3
Jeet Bafna <jeetanil97@gmail.com>
2021-12-22 14:31:23 -0600

    Block pagination backend

--- index.js
+++ index.js
@@ -35,6 +35,24 @@ app.get('/api/blocks', (req, res) => {
 	res.json(blockchain.chain);
 });
 
+app.get('/api/blocks/length', (req, res) => {
+	res.json(blockchain.chain.length);
+});
+
+app.get('/api/blocks/:id', (req, res) => {
+	const { id } = req.params;
+	const { length } = blockchain.chain;
+
+	const blocksReversed = blockchain.chain.slice().reverse();
+
+	let startIndex = (id-1) * 5;
+	let endIndex = id * 5;
+
+	startIndex = startIndex < length ? startIndex : length;
+	endIndex = endIndex < length ? endIndex : length;
+
+	res.json(blocksReversed.slice(startIndex, endIndex));
+});
 
 app.post('/api/mine', (req, res) => {
 	const { data } = req.body;
@@ -157,7 +175,7 @@ if(isDevelopment) {
 	});
 
 
-	for(let i=0; i<10; i++) {
+	for(let i=0; i<20; i++) {
 		if(i%3==0) {
 			walletAction();
 			walletFooAction();
